555849a716fa8408fcbd50aefe3c1ea4e440f3a4
mariordgez <mariordgez@hotmail.com>
2021-07-23 13:59:34 -0700

    Finish unit testing

--- spec/model_spec_.rb
+++ spec/model_spec_.rb
@@ -30,32 +30,101 @@ RSpec.describe User, type: :model do
   end
 end
 
-RSpec.describe Event, type: :model do
-  context 'User with a created event' do
+RSpec.describe Post, type: :model do
+  context 'User with a created post' do
     it 'Should be valid since all validations are true' do
-      user = User.new(email: 'mario@gmail.com', password: '123456')
-      event =
-        user.events.new(
-          title: 'first comment',
-          description: 'first comment',
-          date: Date.current,
+      user =
+        User.new(
+          email: 'mario123456@gmail.com',
+          password: '123456',
+          name: 'mario',
         )
-      expect(event).to be_valid
+      post = user.posts.new(content: 'this is a post')
+      expect(post).to be_valid
     end
-    it 'Should not be valid because its missing title' do
-      user = User.new(email: 'mario@gmail.com', password: '123456')
-      event = user.events.new(description: 'first comment', date: Date.current)
-      expect(event).to_not be_valid
+    it 'Should not be valid because its missing content' do
+      user =
+        User.new(
+          email: 'mario123456@gmail.com',
+          password: '123456',
+          name: 'mario',
+        )
+      post = user.posts.new(content: '')
+      expect(post).to_not be_valid
     end
-    it 'Should not be valid because its missing description' do
-      user = User.new(email: 'mario@gmail.com', password: '123456')
-      event = user.events.new(title: 'first comment', date: Date.current)
-      expect(event).to_not be_valid
+  end
+end
+
+RSpec.describe Like, type: :model do
+  context 'User likes a post' do
+    it 'Should be valid since all validations are true' do
+      user =
+        User.new(
+          id: 1,
+          email: 'mario123456@gmail.com',
+          password: '123456',
+          name: 'mario',
+        )
+      post = user.posts.new(content: 'this is a post', id: 1)
+      like = user.likes.new(user: user, post: post)
+      expect(like).to be_valid
     end
-    it 'Should not be valid because its missing date' do
-      user = User.new(email: 'mario@gmail.com', password: '123456')
-      event = user.events.new(description: 'first comment', title: 'title')
-      expect(event).to_not be_valid
+    it 'Should not be valid since user can only like a post once' do
+      user =
+        User.create(
+          id: 1,
+          email: 'mario123456@gmail.com',
+          password: '123456',
+          name: 'mario',
+        )
+      post = user.posts.create(content: 'this is a post', id: 1)
+      like = user.likes.create(user: user, post: post)
+      like2 = user.likes.create(user: user, post: post)
+      expect(like2).to_not be_valid
+    end
+  end
+end
+
+RSpec.describe Friendship, type: :model do
+  context 'User creates a friendship' do
+    it 'Should be valid since all validations are true' do
+      user1 =
+        User.create(
+          id: 1,
+          email: 'mario123456@gmail.com',
+          password: '123456',
+          name: 'mario',
+        )
+      user2 =
+        User.create(
+          id: 2,
+          email: 'alberto123456@gmail.com',
+          password: '123456',
+          name: 'alberto',
+        )
+      friendship =
+        user1.friendships.build(friend_id: user2.id, confirmed: false)
+      expect(friendship).to be_valid
+    end
+    it 'Should validate inverse friendships' do
+      user1 =
+        User.create(
+          id: 1,
+          email: 'mario123456@gmail.com',
+          password: '123456',
+          name: 'mario',
+        )
+      user2 =
+        User.create(
+          id: 2,
+          email: 'alberto123456@gmail.com',
+          password: '123456',
+          name: 'alberto',
+        )
+      friendship =
+        user1.friendships.create(friend_id: user2.id, confirmed: false)
+      inverse = user2.inverse_friendships.find_by(user_id: user1.id)
+      expect(inverse.user_id).to eql(1)
     end
   end
 end

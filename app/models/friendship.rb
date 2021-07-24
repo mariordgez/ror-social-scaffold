2b5badf67272f9a6fce664a235742dd36a1ab118
mariordgez <mariordgez@hotmail.com>
2021-07-23 18:20:28 -0700

    Add confirm friend method to friendship model

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -3,4 +3,13 @@ class Friendship < ApplicationRecord
   belongs_to :friend, class_name: 'User'
   scope :confirmed_friend, -> { where(confirmed: true) }
   scope :unconfirmed_friend, -> { where(confirmed: false) }
+
+  def confirm_friend
+    self.update_attributes(confirmed: true)
+    Friendship.create!(
+      friend_id: self.user_id,
+      user_id: self.friend_id,
+      confirmed: true,
+    )
+  end
 end
